721afe7063bce7b42a84777f49fbdc789c4221c1
Erfan Momeni <Erfan.Momeni@snapp.cab>
2023-01-21 02:10:02 +0330

    update

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"database/sql"
+	"github.com/jmoiron/sqlx"
+	"user-management/internal/model"
+	"user-management/internal/util"
+)
 
 // NewUserRepository creates a new UserRepository instance.
 func NewUserRepository(db *sqlx.DB) *UserRepository {
@@ -10,4 +15,13 @@ func NewUserRepository(db *sqlx.DB) *UserRepository {
 // UserRepository is the repository for the model.User.
 type UserRepository struct {
 	db *sqlx.DB
+}
+
+// Save creates a model.User into the database.
+func (r UserRepository) Save(user model.User) (sql.Result, error) {
+	q := `INSERT INTO users (id, first_name, last_name, nickname, password, email, country) VALUES (?, ?, ?, ?, ?, ?, ?)`
+
+	uuid := util.GenerateUUId()
+
+	return r.db.Exec(q, uuid, user.FirstName, user.LastName, user.Nickname, user.Password, user.Email, user.Country)
 }
---
+++ internal/util/util.go
@@ -0,0 +1,5 @@
+package util
+
+func GenerateUUId() string {
+	return ""
+}
